3830bd20d0620a48f01a36167a56bc17a7e3d45a
Halyson <halysongoncalves@gmail.com>
2015-02-28 15:00:00 -0300

    Remove files needless

--- pugnotification/src/main/java/br/com/goncalves/pugnotification/constants/BroadcastActions.java
+++
@@ -1,6 +0,0 @@
-package br.com.goncalves.pugnotification.constants;
-
-public class BroadcastActions {
-    public static final String ACTION_PUGNOTIFICATION_CLICK_INTENT = "br.com.mobicare.pugnotification.action.click.intent";
-    public static final String ACTION_PUGNOTIFICATION_DIMISS_INTENT = "br.com.mobicare.pugnotification.action.dismiss.intent";
-}
--- pugnotification/src/main/java/br/com/goncalves/pugnotification/interfaces/PendingIntentNotification.java
+++
@@ -1,9 +0,0 @@
-package br.com.goncalves.pugnotification.interfaces;
-
-import android.app.PendingIntent;
-
-public interface PendingIntentNotification {
-
-    public PendingIntent onSettingPendingIntent();
-
-}
