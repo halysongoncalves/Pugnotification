d4226a5cc455f7b61dd5c194a01ec1c11aed2536
Halyson <halysongoncalves@gmail.com>
2015-03-03 23:28:52 -0300

    Removed website, adiciondo gitattributes and atualizado gitignore.

--- pugnotification/src/main/java/br/com/goncalves/pugnotification/constants/BroadcastActions.java
+++ pugnotification/src/main/java/br/com/goncalves/pugnotification/constants/BroadcastActions.java
@@ -1,6 +1,6 @@
 package br.com.goncalves.pugnotification.constants;
 
 public class BroadcastActions {
-    public static final String ACTION_PUGNOTIFICATION_CLICK_INTENT = "br.com.mobicare.pugnotification.action.click.intent";
-    public static final String ACTION_PUGNOTIFICATION_DIMISS_INTENT = "br.com.mobicare.pugnotification.action.dismiss.intent";
+    public static final String ACTION_PUGNOTIFICATION_CLICK_INTENT = "br.com.goncalves.pugnotification.action.click.intent";
+    public static final String ACTION_PUGNOTIFICATION_DIMISS_INTENT = "br.com.goncalves.pugnotification.action.dismiss.intent";
 }
