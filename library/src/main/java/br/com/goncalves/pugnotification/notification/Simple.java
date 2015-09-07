8a09fb4eb0a47f0f1c4c07aa50f0966dbcb53c60
halysongoncalves <halyson.goncalves@concretesolutions.com.br>
2015-09-07 10:26:15 -0300

    Moved progress information to another type of notification: Progress

--- library/src/main/java/br/com/goncalves/pugnotification/notification/Simple.java
+++ library/src/main/java/br/com/goncalves/pugnotification/notification/Simple.java
@@ -1,39 +1,15 @@
 package br.com.goncalves.pugnotification.notification;
 
-import android.app.Notification;
 import android.support.v4.app.NotificationCompat;
 
 public class Simple extends Builder {
-    private static final String TAG = Simple.class.getSimpleName();
-    private int mProgress;
-    private int mMax;
-    private boolean mIndeterminate;
-
     public Simple(NotificationCompat.Builder builder, int identifier, String tag) {
         super(builder, identifier, tag);
     }
 
     @Override
     public void build() {
-        builder.setProgress(mMax, mProgress, mIndeterminate);
         super.build();
         super.notificationNotify();
-    }
-
-    public Simple update(int identifier) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(PugNotification.mSingleton.mContext);
-        builder.setProgress(mMax, mProgress, mIndeterminate);
-
-        notification = builder.build();
-        notification.flags |= Notification.FLAG_NO_CLEAR;
-        notificationNotify(identifier);
-        return this;
-    }
-
-    public Simple setProgress(int progress, int max, boolean indeterminate) {
-        this.mProgress = progress;
-        this.mMax = max;
-        this.mIndeterminate = indeterminate;
-        return this;
     }
 }
